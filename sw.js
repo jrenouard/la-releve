a7d12f1a78065b04e8102ef689af9cb3576682fc
Julien Renouard <renouard.julien@gmail.com>
2026-07-18 20:20:43 +0200

    Stands, tribunes, pause/sauvegarde et nom du pilote

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="lareleve-v6";
+const C="lareleve-v8";
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(
     ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"])));
