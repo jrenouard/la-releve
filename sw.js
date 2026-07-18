b9f1c5774831c330eafeed9647a585d3a674ff99
Julien Renouard <renouard.julien@gmail.com>
2026-07-18 19:40:05 +0200

    Correction mobile : ecrans de resultats defilables

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="lareleve-v1";
+const C="lareleve-v2";
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(
     ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"])));
